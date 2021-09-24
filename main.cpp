88b29e76081d15e5619bca4db8798bcca0677da4
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2021-09-24 13:43:45 -0500

    main draft

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "Function.h"
+#include "Actor.h"
+#include "Movie.h"
+
+using namespace std;
+
+int main(){
+
+
+
+    return 0;
+}
